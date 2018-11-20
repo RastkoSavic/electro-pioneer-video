ffefdb210068e1f08d1bab28e94768ab83923520
RastkoSavic <rastkosavic23@gmail.com>
2018-11-20 19:06:50 +0100

    Return Duration to Window

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const electron = require("electron");
-const ffmpeg = require("fluent-ffmpeg");
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+let mainWindow;
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+app.on('ready', () => {
+   mainWindow = new BrowserWindow({});
+
+   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on("video:submit", (event, path) => {
-  ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log("File duration is:", metadata.format.duration);
-  });
+ipcMain.on('video:submit', (event, path) => {
+   ffmpeg.ffprobe(path, (err, metadata) => {
+      mainWindow.webContents.send('video:metadata', metadata.format.duration);
+   });
 });
